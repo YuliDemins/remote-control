4332a5d90bdf0038e781d6e583eb3afca8e05c45
Yulia <yudemins@mail.ru>
2023-05-21 22:26:42 +0300

    feat: add command in front

--- index.ts
+++ index.ts
@@ -33,24 +33,31 @@ socket.on('connection', async (connection) => {
     }
     if (command == 'mouse_up') {
       await mouse.move(up(number));
+      connection.send(command)
     }
     if (command == 'mouse_down') {
       await mouse.move(down(number));
+      connection.send(command)
     }
     if (command == 'mouse_left') {
       await mouse.move(left(number));
+      connection.send(command)
     }
     if (command == 'mouse_right') {
       await mouse.move(right(number));
+      connection.send(command)
     }
     if (command == 'draw_square') {
       drawShape(number, number)
+      connection.send(command)
     }
     if (command == 'draw_rectangle') {
       drawShape(number, +args[0])
+      connection.send(command)
     }
     if (command == 'draw_circle') {
       drawCircle(number)
+      connection.send(command)
     }
     if (command == 'prnt_scrn') {
       let point = await mouse.getPosition()
