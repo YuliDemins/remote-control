adb324b05a680b2c2cc959484c5c5eaae58e6e53
Yulia <yudemins@mail.ru>
2023-05-21 13:58:24 +0300

    fix: change draw circle

--- src/service/drawCircle.ts
+++ src/service/drawCircle.ts
@@ -1,16 +1,16 @@
-import { Button, Point, mouse } from "@nut-tree/nut-js";
+import { mouse } from "@nut-tree/nut-js";
 
 export const drawCircle = async (radius: number) => {
-  const step = 0.01 * Math.PI;
+  const step = 0.01
   let point = await mouse.getPosition()
   let xStart = point.x;
   let yStart = point.y;
+  let points: {x:number, y:number}[] = []
   
-  for (let i = 0; i < Math.PI * 2; i += step) {
+  for (let i = 0; i <= Math.PI * 2; i += step) {
     let x = xStart - radius + (radius * Math.cos(i));
     let y = yStart + (radius * Math.sin(i));
-    await mouse.pressButton(Button.LEFT)
-    await mouse.setPosition(new Point(x, y));
+    points.push({x,y})
   }
-  await mouse.releaseButton(Button.LEFT)
+  await mouse.drag(points);
 }
